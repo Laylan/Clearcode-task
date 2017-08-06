3bffb53c4b82bb42b2bd90681079ecfdb9127e6f
Laylan <anka_anka00@vp.pl>
2017-08-06 17:34:34 +0200

    different views

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 (function(){
   var app = angular.module('myRedditApp', ['app-directives']);
-console.log("articles Ctrl11");
-  // app.controller('RedditController',['$scope' ,'$http', '$log', function($scope, $http, $log){
-  //   console.log("reddits Ctrl");
-  //   $http.get("http://www.reddit.com/new.json?sort=new")
-  //   .then(function(success) {
-  //     console.log(success);
-  //   });
-    // .error(function(returnData) {
-    //   console.log("error Http", returnData);
-    // });
-  // } ]);
+  // app.config(['$routeProvider',
+  //   function($routeProvider) {
+  //     $routeProvider.
+  //       when('/article-view', {
+  // 	templateUrl: 'views/article-view.html'
+  // 	// controller: 'AddOrderController'
+  //       }).
+  //       otherwise({
+  // 	redirectTo: '/articles-view'
+  //       });
+  // }]);
+
+
+  // sampleApp.controller('AddOrderController', function($scope) {
+  //
+  // 	$scope.message = 'This is Add new order screen';
+  //
+  // });
+  //
+  //
+  // sampleApp.controller('ShowOrdersController', function($scope) {
+  //
+  // 	$scope.message = 'This is Show orders screen';
+  //
+  // });
+
 })();
--- js/articles.js
+++ js/articles.js
@@ -1,44 +1,64 @@
 (function (){
-  var app = angular.module('app-directives', []);
+  var app = angular.module('app-directives', ['ngRoute']);
 
-  app.directive("articlesView", function () {
-    console.log("app-directives");
-    var articlesss = [
-      {
-        title : 'Dogs',
-        author : 'Whoof, Whoof, Whoof',
-     },
-      {
-        title: "Cats",
-        author: "Miau Miau",
-      }
-    ];
-    return {
-      restrict: "E",
-      templateUrl: "views/articles-view.html",
-      controller: function($http) {
-        //this.reddits = articlesss;
-        var vm = this;
-        vm.reddits = [];
-        $http.get("http://www.reddit.com/new.json?sort=new")
-        .then(function FetchNewReddits(returnedData) {
-          console.log(returnedData.data.data.children);
-          vm.reddits = returnedData.data.data.children;
-        },
-        function FetchNewRedditsError(error) {
-         console.log(error);
-       });
-      },
+  app.config(['$routeProvider', '$locationProvider',
+    function($routeProvider) {
+      $routeProvider
+        .when('/views/article-view', {
+    templateUrl: '/views/article-view.html'
+    // controller: 'AddOrderController'
+  })
+        .when('/views/articles-view', {
+      templateUrl: '/views/articles-view.html',
+      controller: "ArticlesCtrl",
       controllerAs: "reddit",
-    };
-  });
+      })
+      .otherwise({
+     redirectTo: 'views/articles-view'
+         });
+  }]);
 
-  app.directive("articleView", function () {
-    console.log("#####");
-    return {
-      restrict: "E",
-      templateUrl: "views/article-view.html"
-    };
-  });
+  // app.directive("articlesView",
+  // function () {
+  //   return {
+  //     restrict: "E",
+  //     templateUrl: "/views/articles-view.html",
+  //     controller: "ArticlesCtrl",
+  //     controllerAs: "reddit",
+  //   };
+  // });
+
+app.controller("ArticlesCtrl", function($scope, $route, $http, $location) {
+  //this.reddits = articlesss;
+  var vm = this;
+  vm.reddits = [];
+  $http.get("http://www.reddit.com/new.json?sort=new")
+  .then(function FetchNewReddits(returnedData) {
+    console.log(returnedData.data.data.children);
+    vm.reddits = returnedData.data.data.children;
+  },
+  function FetchNewRedditsError(error) {
+   console.log(error);
+ });
+
+ $scope.showArticle = function() {
+   console.log("Change view func");
+   console.log($route);
+  //  $scope.isArticle = true;
+   $location.url('/views/article-view');
+ };
+
+//  $routeProvider.when('/views/article-view', {
+//    templateUrl: 'views/article-view.html'
+// });
+});
+
+  // app.directive("articleView", function () {
+  //   console.log("#####");
+  //   return {
+  //     restrict: "E",
+  //     templateUrl: "views/article-view.html"
+  //   };
+  // });
 
 })();
