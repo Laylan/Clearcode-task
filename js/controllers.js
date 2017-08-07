e322a55c2aa3dfd6fbe64624d8a5026f17b1683c
Laylan <anka_anka00@vp.pl>
2017-08-07 11:20:35 +0200

    nacigation

--- js/controllers.js
+++ js/controllers.js
@@ -10,6 +10,17 @@
       },
       setParam: function(value) {
           self.article = value;
+      },
+      setPage: function(articles, page) {
+        self.articles = articles;
+        self.page = page;
+      },
+      getArticles: function() {
+        return self.articles;
+      },
+      getPage: function() {
+        console.log(self.page);
+        return self.page;
       }
     }
 
@@ -20,37 +31,85 @@
     var vm = this;
     vm.reddits = [];
     vm.article_id = "";
-    $http.get("http://www.reddit.com/r/Articles/new.json?sort=new")
-    .then(function FetchNewReddits(returnedData) {
-      console.log(returnedData.data.data.children);
-      vm.reddits = returnedData.data.data.children;
-    },
-    function FetchNewRedditsError(error) {
-     console.log(error);
-    });
+    vm.page = 0;
+    if(paramsService.getArticles()) {
+      vm.reddits = paramsService.getArticles();
+      vm.page = paramsService.getPage();
+    }
+    else {
+      $http.get("http://www.reddit.com/r/Articles/new.json?sort=new")
+      .then(function FetchNewReddits(returnedData) {
+        console.log(returnedData.data.data.children);
+        vm.reddits = returnedData.data.data.children;
+      },
+      function FetchNewRedditsError(error) {
+       console.log(error);
+      });
+    }
+
 
     $scope.showArticle = function(article) {
-     console.log("Change view func");
      paramsService.setParam(article);
-     $window.localStorage.setItem(article, article);
+     paramsService.setPage(vm.reddits, vm.page);
+     this.article = article;
+    //  $window.localStorage.setItem(title, this.article.data.title);
+    //  $window.localStorage.setItem(content, this.article.data.selfText);
+    //  $window.localStorage.setItem(url, this.article.data.url);
      console.log($window.localStorage.getItem(article));
      console.log(paramsService);
      $scope.isArticle = true;
      $location.url('/views/article-view');
     };
+
+    $scope.nextPage = function() {
+      vm.page += 25;
+      var after = vm.reddits[24].data.name;
+      console.log(vm.page);
+      $http.get("http://www.reddit.com/r/Articles/new.json?count=" + vm.page + "&after=" + after)
+      .then(function FetchNewReddits(returnedData) {
+        console.log(returnedData.data.data.children);
+        vm.reddits = returnedData.data.data.children;
+      },
+      function FetchNewRedditsError(error) {
+       console.log(error);
+      });
+    }
+      $scope.previousPage = function() {
+        vm.page -= 25;
+        var before = vm.reddits[0].data.name;
+        console.log(vm.page);
+        $http.get("http://www.reddit.com/r/Articles/new.json?count=" + vm.page + "&before=" + before)
+        .then(function FetchNewReddits(returnedData) {
+          console.log(returnedData.data.data.children);
+          vm.reddits = returnedData.data.data.children;
+        },
+        function FetchNewRedditsError(error) {
+         console.log(error);
+        });
+    }
   }]);
 
-  app.controller("ArticleCtrl", ['$scope', '$http', '$location','paramsService',
-  function($scope,$http, $location, paramsService) {
+  app.controller("ArticleCtrl", ['$scope', '$http', '$location', '$window','paramsService',
+  function($scope,$http, $location, $window, paramsService) {
     var vm = this;
     vm.article = {};
     vm.msg = "";
+    vm.local = {};
     vm.article = paramsService.getParam();
     console.log(vm.article);
-    if(!vm.article.data.selfText) {
+    if(vm.article === undefined) {
+      alert("page reloaded!");
+      console.log($window.localStorage);
+      // vm.local.title = $window.localStorage.getItem(title);
+      // vm.local.content = $window.localStorage.getItem(content);
+      // vm.local.url = $window.localStorage.getItem(url);
+    }
+    else {
+      if(!vm.article.data.selfText) {
         vm.msg = "This is an article in external domain. You can see the article using a link:";
+      }
     };
-
+    console.log($scope);
     $scope.backToArticles = function () {
       console.log("Back to main view");
       $location.url('/views/articles-view');
--- readMe.md
+++ readMe.md
@@ -1,11 +1,14 @@
-Junior Front-end Dev App \n
-Shows articles from Reddit.com
+Junior Front-end Dev App <br/>
+Shows a list of articles from Reddit.com, you can read them if they are in reddit.com domain. Otherwise there is a link to an artcile.<br/>
+There is a form to comment an article but it doesn't really do anything.
 
 To start a serwer:
 1. Download and install nodeJs
 2. Command line: npm install -g http-server
-3. Start serwer: http-server
+3. In command line to start serwer: http-server (if problem see at google how to start a http-server)
+4. In a web browser: localhost:8080
 
 App uses:
 - Bootstrap
 - AbgularJS
+- jQuery
